946e7153f615704cd0c3e1251ee055d6ca379f46
Aaryan Gotad <69253632+AaryanGotad@users.noreply.github.com>
2026-07-21 15:02:07 +0000

    started learning about streamlit widgets

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,39 @@ st.write(df)
 # using numpy to generate a df
 numpy_df = np.random.randn(10, 20)
 st.dataframe(numpy_df)
+
+# highlighting elemnents in the df
+pandas_df = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20))
+)
+st.dataframe(pandas_df.style.highlight_max(axis=0))
+
+# static table generation
+st.table(pandas_df)
+
+# DRAW A LINE CHART
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c']
+)
+
+st.line_chart(chart_data)
+
+# PLOT A MAP
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon']
+)
+
+st.map(map_data)
+
+# WIDGETS: It's really straightforward, think of widgets as variables
+"""
+When you've got the data or model into the state that you want to explore,
+you can add in widgets like st.slider(), st.button() or st.selectbox().
+It's really straightforward — treat widgets as variables:
+"""
+
+x = st.slider('x') # 👈 this is a widget
+st.write(x, 'squared is', x * x)
